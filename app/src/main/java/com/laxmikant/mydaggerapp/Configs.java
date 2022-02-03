38de90cbf0fdb0122e987bb0f5763bb63c27dc70
laxmikant.mahamuni <mahamunilaxmikant@gmail.com>
2022-02-03 16:22:32 +0530

    Update

--- app/src/main/java/com/laxmikant/mydaggerapp/Configs.java
+++ app/src/main/java/com/laxmikant/mydaggerapp/Configs.java
@@ -2,13 +2,13 @@ package com.laxmikant.mydaggerapp;
 
 public final class Configs {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.bookatable.android.debug";
+  public static final String APPLICATION_ID = "com.laxmikant.mydaggerapp.debug";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "dev";
   public static final int VERSION_CODE = 225;
   public static final String VERSION_NAME = "5.0.0";
   // Fields from product flavor: dev
-  public static final String API_KEY = "a1i6nZbIxVanfyIgCBr3u7oSWAl4jPBQ5uBwbeHQ";
-  public static final String BASE_URL = "https://uat-gw.bookatable.com/mobile/v1/";
+  public static final String API_KEY = "";
+  public static final String BASE_URL = "https://www.google.com/";
   public static final String ENVIRONMENT = "DEV";
 }
